28ed1ef6cbd5cf901ebf822f0d0c5d00e225ac92
data1225 <data.analysis.1225@gmail.com>
2025-08-28 22:47:54 +0800

    修改程式

--- application/fetch_youtube_trending_by_work.py
+++ application/fetch_youtube_trending_by_work.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[ ]:
 
 
 
@@ -20,6 +20,7 @@ from etl_showcase.domain.models import (
 from etl_showcase.domain.youtube_models import (
     YoutubeVideo, 
     Topic,
+    TopicDetail,
     VideoSearchOrder,
 )
 from etl_showcase.infrastructure.utils.time_utils import get_previous_month_range_in_utc
@@ -46,21 +47,17 @@ search_youtube_result = BaseResponse[YoutubeVideo](
     content=None
 )    
 start_utc_datetime, end_utc_datetime = get_previous_month_range_in_utc()
-# # 縮小搜尋量供測試用，因youtube API有搜尋上限
-# topics = [
-#     Topic('科技議題', ['AI']),
-# ]
 topics = [
-    Topic('藏海傳-zh', ['藏海傳 影評']),
-    Topic('藏海傳-en', ['Zang Hai Zhuan review']),
-    Topic('琅琊榜-zh', ['琅琊榜 影評']),
-    Topic('琅琊榜-en', ['Nirvana in Fire review']),
-    Topic('慶餘年-zh', ['慶餘年 影評']),
-    Topic('慶餘年-en', ['Joy of Life review']),
+    Topic('藏海傳-zh', [TopicDetail('藏海傳 影評')]),
+    Topic('藏海傳-en', [TopicDetail('Zang Hai Zhuan review')]),
+    Topic('琅琊榜-zh', [TopicDetail('琅琊榜 影評')]),
+    Topic('琅琊榜-en', [TopicDetail('Nirvana in Fire review')]),
+    Topic('慶餘年-zh', [TopicDetail('慶餘年 影評')]),
+    Topic('慶餘年-en', [TopicDetail('Joy of Life review')]),
 ]
 
 for topic in topics:
-    for keyword in topic.keywords:
+    for detail in topic.details:
         # Determine the language based on topic name
         if 'en' in topic.name:
             relevanceLanguage = LanguageCode.English
@@ -68,15 +65,15 @@ for topic in topics:
             relevanceLanguage = LanguageCode.Chinese
 
         search_youtube_result = youtube_search_videos(
-            query=keyword,
+            query=detail.keyword,
             search_count=50,
             order=VideoSearchOrder.RELEVANCE,
             relevanceLanguage=relevanceLanguage
         )
-        print(f'keyword: {keyword}, result: [{search_youtube_result.status_code}] {search_youtube_result.message}')
+        print(f'keyword: {detail.keyword}, result: [{search_youtube_result.status_code}] {search_youtube_result.message}')
         if search_youtube_result.content is None:
             break
-        topic.add_youtube_videos(search_youtube_result.content)
+        detail.add_youtube_videos(search_youtube_result.content)
 
 # update log and data in google sheets
 write_secret_json()
@@ -100,19 +97,19 @@ try:
                     "Video ID", "Video URL", "Video title", "Video description", 
                     "Publish datetime", "Channel name", "Channel ID", "Thumbnail URL"]]
     for topic in topics:
-        keywords_string = '、'.join(topic.keywords)
-        for video in topic.youtube_videos:
-            update_rows.append([
-                topic.name,
-                keywords_string,
-                video.id,
-                f"https://www.youtube.com/watch?v={video.id}",
-                video.title,
-                video.description,
-                video.published_at,
-                video.channel_title,
-                video.channel_id,
-                video.thumbnail_url
+        for detail in topic.details:
+            for video in detail.youtube_videos:
+                update_rows.append([
+                    topic.name,
+                    detail.keyword,
+                    video.id,
+                    f"https://www.youtube.com/watch?v={video.id}",
+                    video.title,
+                    video.description,
+                    video.published_at,
+                    video.channel_title,
+                    video.channel_id,
+                    video.thumbnail_url
             ])
     # update google sheet
     update_sheet_result = update_full_google_sheet(
@@ -127,19 +124,20 @@ try:
     # transform original data to table
     update_rows = [["Topic ", "Github action variable"]]
     for topic in topics:
-        video_ids = [video.id for video in topic.youtube_videos]
-        # 建立一個 Python 字典，包含 topic.name 和 video_ids
-        github_action_variable = {
-            "screenwork_name": topic.name,
-            "video_ids": video_ids
-        }     
-        # 將字典轉換為 JSON 字串
-        github_action_json_string = json.dumps(github_action_variable, ensure_ascii=False)   
-        # 根據新的結構更新陣列
-        update_rows.append([
-            topic.name,
-            github_action_json_string
-        ])
+        for detail in topic.details:
+            video_ids = [video.id for video in detail.youtube_videos]
+            # 建立一個 Python 字典，包含 topic.name 和 video_ids
+            github_action_variable = {
+                "screenwork_name": topic.name,
+                "video_ids": video_ids
+            }     
+            # 將字典轉換為 JSON 字串
+            github_action_json_string = json.dumps(github_action_variable, ensure_ascii=False)   
+            # 根據新的結構更新陣列
+            update_rows.append([
+                topic.name,
+                github_action_json_string
+            ])
     # update google sheet
     update_sheet_result = update_full_google_sheet(
         spreadsheet_id = YOUTUBE_SPREADSHEET_ID,
